4fd68a5a7367e4ca532e979e88aa690356ad3472
HUMBERTO JOSE DANTAS <humbertojdantas@yahoo.com.br>
2023-05-04 16:11:58 -0300

    firstProject - Java

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Screen Match");
+        System.out.println("Filme: O Resgate do Soldado Ryan");
+
+        int anoDeLancamento = 1998;
+        System.out.println("Ano de Lançamento: " + anoDeLancamento);
+        boolean incluidoNoPlano = true;
+        double notaDoFilme = 9.4;
+
+        double media = (9.8 + 6.3 + 8.0) / 3;
+        System.out.println(media);
+        String sinopse;
+        sinopse = "Filme sobre a Segunda Guerra Mundial";
+        System.out.println(sinopse);
+
+        int classificacao;
+        classificacao = (int) (media / 2);
+        System.out.println("Filme com classificação de " + classificacao + " estrelas");
+
+    }
+}
+
